2bd692e1ff666702b14c01b88c8f80ae80828e2f
Tim Lapinskas <tim.lapinskas@gmail.com>
2017-06-26 21:53:22 -0700

    Added functionality to build table dynamically in DOM

--- app/assets/javascripts/sportradar.js
+++ app/assets/javascripts/sportradar.js
@@ -1,41 +1,35 @@
-$(document).ready(function() {
+$(document).ready(function worker() {
 	$.ajax({
 	  type: "GET",
 	  url: "/sportradar/mlbSportradar",
 	  dataType: "JSON",
-	  // data: '@data',
 	  success: function(data) {
 	    console.log(data);
-	  }
+		  $.each(data, function(key, val) {
+				$( "#container" ).append('<table>');
+				$( "#container" ).append('<tr>');
+				$( "#container" ).append('<th>Team</th>');
+				$( "#container" ).append('<th>Runs</th>');
+				$( "#container" ).append('<th>Hits</th>');
+				$( "#container" ).append('<th>Errors</th>');
+				$( "#container" ).append('</tr>');
+				$( "#container" ).append('<tr>');
+				$( "#container" ).append("<td id=''"+key+"''>"+val.home_name+"</td>");
+				$( "#container" ).append("<td id=''"+key+"''>"+val.home_runs+"</td>");
+				$( "#container" ).append("<td id=''"+key+"''>"+val.home_hits+"</td>");
+				$( "#container" ).append("<td id=''"+key+"''>"+val.home_errors+"</td>");
+				$( "#container" ).append("</tr>");
+				$( "#container" ).append("<tr>");
+				$( "#container" ).append("<td id=''"+key+"''>"+val.away_name+"</td>");
+				$( "#container" ).append("<td id=''"+key+"''>"+val.away_runs+"</td>");
+				$( "#container" ).append("<td id=''"+key+"''>"+val.away_hits+"</td>");
+				$( "#container" ).append("<td id=''"+key+"''>"+val.away_errors+"</td>");
+				$( "#container" ).append("</tr>");
+				$( "#container" ).append("</table>");
+			});
+		},
+		// complete: function() {
+		// 	setTimeout(worker, 5000);
+	 //  }
 	});
 });
-
-// $(document).ready(function() {
-// 	$(function() {
-// 		$.getJSON("/sportradar/mlbSportradar", function(data){
-// 			var items = [];
-// 			$.each(data, function(key, val) {
-// 				items.push('<table>');
-// 				items.push('<tr>');
-// 				items.push('<th>Team"</th>');
-// 				items.push('<th>Runs"</th>');
-// 				items.push('<th>Hits"</th>');
-// 				items.push('<th>Errors"</th>');
-// 				items.push('</tr>');
-// 				items.push('<tr>');
-// 				items.push("<td id=''"+key+"''>"+val.home_name+"</td>");
-// 				items.push("<td id=''"+key+"''>"+val.home_runs+"</td>");
-// 				items.push("<td id=''"+key+"''>"+val.home_hits+"</td>");
-// 				items.push("<td id=''"+key+"''>"+val.home_errors+"</td>");
-// 				items.push("</tr>");
-// 				items.push("<tr>");
-// 				items.push("<td id=''"+key+"''>"+val.away_name+"</td>");
-// 				items.push("<td id=''"+key+"''>"+val.away_runs+"</td>");
-// 				items.push("<td id=''"+key+"''>"+val.away_hits+"</td>");
-// 				items.push("<td id=''"+key+"''>"+val.away_errors+"</td>");
-// 				items.push("</tr>");
-// 				items.push("</table>");
-// 			});
-// 		});
-// 	});
-// });
